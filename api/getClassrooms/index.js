f1a8b510572ecad96ed2667884fd8a71eb6cd791
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2025-05-02 20:57:50 +0300

    Update index.js

--- api/getClassrooms/index.js
+++ api/getClassrooms/index.js
@@ -1,28 +1,6 @@
-const sql = require('mssql');
-
 module.exports = async function (context, req) {
-  try {
-    await sql.connect(process.env.DB_CONNECTION_STRING);
-
-    const dbRequest = new sql.Request();
-    dbRequest.input('status', sql.VarChar(50), 'Current');
-
-    const result = await dbRequest.query(`
-      SELECT Id, Name
-      FROM Classrooms
-      WHERE Status = @status
-    `);
-
     context.res = {
-      status: 200,
-      body: result.recordset
+        status: 200,
+        body: { message: "API is working!" }
     };
-  } 
-  catch (err) {
-    context.log.error('DB failure:', err);
-    context.res = {
-      status: 500,
-      body: { error: 'Internal server error.' }
-    };
-  }
 };
