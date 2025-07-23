dca53234931bcde0a82a7027d826639867b726c9
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2025-07-23 15:46:10 +0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const sql = require('mssql');
+module.exports = async function (context, req) {
+  try {
+    const connectionString = process.env.DB_CONNECTION_STRING;
+    await sql.connect(connectionString);
+    const dbRequest = new sql.Request();
+    dbRequest.input('status', sql.VarChar(50), 'Current');
+    const result = await dbRequest.query(`
+      SELECT Id, Name
+      FROM Classroom
+      WHERE Status = @status
+    `);
+    context.res = {
+      status: 200,
+      body: result.recordset,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+  } 
+  catch (err) {
+    context.res = {
+      status: 500,
+      body: { error: 'Internal Server Error' },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+  }
+};
